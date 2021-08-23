24a98ccf4dbd9d6e1e567f3f2296f0823121d715
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-08-23 23:42:58 +0800

    Add new task feature

--- frontend/src/views/ListDetail/index.js
+++ frontend/src/views/ListDetail/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const data = {
   title: "To-do list",
@@ -23,12 +27,13 @@ const data = {
 
 const ListDetail = () => {
   const [tasks, setTasks] = useState([]);
+  const [newRows, setNewRows] = useState([]);
 
   useEffect(() => {
     setTasks(data.tasks);
   }, []);
 
-  const handleChange = (e, id) => {
+  const handleCheck = (e, id) => {
     const currTaskIdx = tasks.findIndex((task) => task.id == id);
     const newState = !tasks[currTaskIdx].isComplete;
     setTasks((prevState) => [
@@ -41,6 +46,43 @@ const ListDetail = () => {
     ]);
   };
 
+  const handleAddRow = () => {
+    setNewRows((prevState) => [
+      ...prevState,
+      {
+        id: null,
+        isComplete: false,
+        content: "",
+      },
+    ]);
+  };
+
+  const handleAddTask = (idx) => {
+    setNewRows((prevState) => [
+      ...prevState.slice(0, idx),
+      {
+        ...prevState[idx],
+        id: idx + 1,
+        timestamp: new Date(),
+      },
+      ...prevState.slice(idx + 1),
+    ]);
+  };
+
+  const handleNewTaskContent = (e, id) => {
+    const currTaskIdx = newRows.findIndex((task) => task.id == id);
+    setNewRows((prevState) => [
+      ...prevState.slice(0, currTaskIdx),
+      {
+        ...prevState[currTaskIdx],
+        content: e.target.value,
+      },
+      ...prevState.slice(currTaskIdx + 1),
+    ]);
+  };
+
+  console.log(newRows);
+
   return (
     <Layout>
       <h1 className="text-center">{data.title}</h1>
@@ -52,7 +94,7 @@ const ListDetail = () => {
           <tr>
             <th>Completed</th>
             <th>Task</th>
-            <th>Timestamp</th>
+            <th>Date Created</th>
           </tr>
         </thead>
         <tbody>
@@ -62,15 +104,51 @@ const ListDetail = () => {
                 <input
                   type="checkbox"
                   checked={task.isComplete}
-                  onChange={(e) => handleChange(e, task.id)}
+                  onChange={(e) => handleCheck(e, task.id)}
                 />
               </td>
               <td>{task.content}</td>
               <td>{task.timestamp.toLocaleDateString("en-US")}</td>
             </tr>
           ))}
+          {newRows.map((task, idx) =>
+            task.id ? (
+              <tr>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={task.isComplete}
+                    onChange={(e) => handleCheck(e, task.id)}
+                  />
+                </td>
+                <td>{task.content}</td>
+                <td>{task.timestamp.toLocaleDateString("en-US")}</td>
+              </tr>
+            ) : (
+              <tr>
+                <td></td>
+                <td>
+                  <Row>
+                    <Col>
+                      <FormControl
+                        type="text"
+                        placeholder="Task Name"
+                        value={newRows[idx].content}
+                        onChange={handleNewTaskContent}
+                      />
+                    </Col>
+                    <Col>
+                      <Button onClick={() => handleAddTask(idx)}>Done</Button>
+                    </Col>
+                  </Row>
+                </td>
+                <td></td>
+              </tr>
+            )
+          )}
         </tbody>
       </Table>
+      <Button onClick={handleAddRow}>+ Add Task</Button>
     </Layout>
   );
 };
