5a12f2155fda21661e9d56730eaa8b8995834e63
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-08-23 23:46:14 +0800

    Make card a hyperlink

--- frontend/src/components/CardComponent/index.js
+++ frontend/src/components/CardComponent/index.js
@@ -1,25 +1,28 @@
 import Card from "react-bootstrap/Card";
+import Link from "../Link";
 
 const CardComponent = ({ data }) => {
   const { title, completedTasks, incompleteTasks, dateCreated } = data;
   return (
-    <Card className="w-100">
-      <Card.Header as="h5" className="text-center">
-        {title}
-      </Card.Header>
-      <Card.Body>
-        <Card.Subtitle className="mb-2 text-muted text-center">
-          Date Created: {dateCreated.toLocaleDateString("en-US")}
-        </Card.Subtitle>
-        <Card.Text>
-          Completed Tasks: {completedTasks}
-          <br />
-          Incomplete Tasks: {incompleteTasks}
-        </Card.Text>
-        {/* <Card.Link href="#">Card Link</Card.Link>
+    <Link to={`/list/${data.id}`}>
+      <Card className="w-100">
+        <Card.Header as="h5" className="text-center">
+          {title}
+        </Card.Header>
+        <Card.Body>
+          <Card.Subtitle className="mb-2 text-muted text-center">
+            Date Created: {dateCreated.toLocaleDateString("en-US")}
+          </Card.Subtitle>
+          <Card.Text>
+            Completed Tasks: {completedTasks}
+            <br />
+            Incomplete Tasks: {incompleteTasks}
+          </Card.Text>
+          {/* <Card.Link href="#">Card Link</Card.Link>
         <Card.Link href="#">Another Link</Card.Link> */}
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -5,12 +5,14 @@ import Col from "react-bootstrap/Col";
 
 const data = [
   {
+    id: 1,
     title: "To-Do List",
     completedTasks: 4,
     incompleteTasks: 2,
     dateCreated: new Date(2021, 8, 23, 10, 33, 30, 0),
   },
   {
+    id: 2,
     title: "To-Do List",
     completedTasks: 4,
     incompleteTasks: 2,
@@ -27,7 +29,6 @@ const Home = () => {
       </Col>
     );
 
-    // force wrap to next row every 42cards
     if ((idx + 1) % 2 === 0) {
       cards.push(<div className="w-100 my-2"></div>);
     }
@@ -35,12 +36,8 @@ const Home = () => {
 
   return (
     <Layout>
+      <h1 className="text-center">My Lists</h1>
       <Row className="my-4 px-4">{cards}</Row>
-      {/* <div className="d-flex flex-column justify-content-center align-items-center my-4 px-4">
-        {data.map((card) => (
-          <CardComponent data={card} />
-        ))}
-      </div> */}
     </Layout>
   );
 };
